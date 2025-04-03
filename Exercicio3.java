dc7f911503a5fda7974071f1c7a9545fc0a03b69
Pedro Arthur <59922544+PedroArthurHartmann@users.noreply.github.com>
2025-04-03 00:45:49 +0000

    coisas nao commitadas

--- Exercicio3.java
+++ Exercicio3.java
@@ -1,12 +1,12 @@
 import java.util.*;
 
 public class Exercicio3 {
-    static List<Item> mochila(List<Item> opt) {
+    static List<Item> mochila(List<Item> opt, int cap) {
         List<Item> result = new ArrayList<>();
         Queue<Item> items = new PriorityQueue<>((i, j) -> ((Float)(j.value/j.weight)).compareTo(i.value/i.weight));
 
         items.addAll(opt);
-        while (result.stream().mapToDouble(i -> i.weight).sum() < 5) {
+        while (result.stream().mapToDouble(i -> i.weight).sum() <= cap) {
             result.add(items.remove());
         }
 
@@ -14,7 +14,26 @@ public class Exercicio3 {
     }
 
     public static void main(String[] args) {
-        System.out.println(mochila(Arrays.asList(new Item(4,12), new Item(2,1), new Item(10,4), new Item(1,1), new Item(2,2))));
+        /*System.out.println(mochila(Arrays.asList(
+            new Item(92, 23),
+            new Item(57, 31),
+            new Item(49, 29),
+            new Item(68, 44),
+            new Item(60, 53),
+            new Item(43, 38),
+            new Item(67, 63),
+            new Item(84, 85),
+            new Item(87, 89),
+            new Item(72, 82))
+            ,165)));*/
+        System.out.println(mochila(Arrays.asList(
+            new Item(50, 56),
+            new Item(50, 59),
+            new Item(64, 80),
+            new Item(46, 64),
+            new Item(50, 75),
+            new Item(5, 17)
+        ), 190));
     }
 }
 
